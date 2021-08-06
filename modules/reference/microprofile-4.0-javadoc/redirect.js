fdfc3e7d9fa1e389980bf539ad9db1e9747fe0ba
Natalie Bernhard <natalie.bernhard@ibm.com>
2021-08-06 10:14:29 -0400

    adding redirect

--- modules/reference/microprofile-4.0-javadoc/redirect.js
+++ modules/reference/microprofile-4.0-javadoc/redirect.js
@@ -4,6 +4,18 @@ $(function () {
       "/docs/modules/reference/microprofile-4.0-javadoc"
     )
   ) {
-    console.log("here");
+    var jd = window.top.location.href;
+    jd = jd.substring(jd.indexOf("microprofile-4.0-javadoc") + 25, 0);
+    var pack = jd.substring(0, jd.lastIndexOf("/"));
+    var port =
+      window.top.location.port !== "" ? ":" + window.top.location.port : "";
+    window.top.location.href =
+      "https://" +
+      window.top.location.hostname +
+      port +
+      "/docs/ref/microprofile/4.0/#package=" +
+      pack +
+      "/package-frame.html&class=" +
+      jd;
   }
 });
